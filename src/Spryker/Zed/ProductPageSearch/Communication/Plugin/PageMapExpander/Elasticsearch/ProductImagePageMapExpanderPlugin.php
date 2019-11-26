da51ea922fff8f47e0cb60961d0ee47b728590c5
Pavlo Asaulenko <asaulenko.pavel@gmail.com>
2019-11-26 20:31:44 +0200

    TE-4207 Reimplemented mapping logic

--- src/Spryker/Zed/ProductPageSearch/Communication/Plugin/PageMapExpander/Elasticsearch/ProductImagePageMapExpanderPlugin.php
+++ src/Spryker/Zed/ProductPageSearch/Communication/Plugin/PageMapExpander/Elasticsearch/ProductImagePageMapExpanderPlugin.php
@@ -5,29 +5,32 @@
  * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
  */
 
-namespace Spryker\Zed\ProductPageSearch\Communication\Plugin\PageMapExpander;
+namespace Spryker\Zed\ProductPageSearch\Communication\Plugin\PageMapExpander\Elasticsearch;
 
 use Generated\Shared\Transfer\LocaleTransfer;
 use Generated\Shared\Transfer\PageMapTransfer;
 use Spryker\Zed\Kernel\Communication\AbstractPlugin;
-use Spryker\Zed\ProductPageSearch\Dependency\Plugin\ProductPageMapExpanderInterface;
-use Spryker\Zed\Search\Business\Model\Elasticsearch\DataMapper\PageMapBuilderInterface;
+use Spryker\Zed\ProductPageSearchExtension\Dependency\PageMapBuilderInterface;
+use Spryker\Zed\ProductPageSearchExtension\Dependency\Plugin\ProductAbstractPageMapExpanderPluginInterface;
 
 /**
- * @deprecated Use `\Spryker\Zed\ProductPageSearch\Communication\Plugin\PageMapExpander\Elasticsearch\ProductImagePageMapExpanderPlugin` instead.
- *
  * @method \Spryker\Zed\ProductPageSearch\Persistence\ProductPageSearchQueryContainerInterface getQueryContainer()
  * @method \Spryker\Zed\ProductPageSearch\Communication\ProductPageSearchCommunicationFactory getFactory()
  * @method \Spryker\Zed\ProductPageSearch\Business\ProductPageSearchFacadeInterface getFacade()
  * @method \Spryker\Zed\ProductPageSearch\ProductPageSearchConfig getConfig()
  */
-class ProductImagePageMapExpanderPlugin extends AbstractPlugin implements ProductPageMapExpanderInterface
+class ProductImagePageMapExpanderPlugin extends AbstractPlugin implements ProductAbstractPageMapExpanderPluginInterface
 {
+    protected const KEY = 'images';
+    protected const VALUE = 'product_images';
+
     /**
+     * {@inheritDoc}
+     *
      * @api
      *
      * @param \Generated\Shared\Transfer\PageMapTransfer $pageMapTransfer
-     * @param \Spryker\Zed\Search\Business\Model\Elasticsearch\DataMapper\PageMapBuilderInterface $pageMapBuilder
+     * @param \Spryker\Zed\ProductPageSearchExtension\Dependency\PageMapBuilderInterface $pageMapBuilder
      * @param array $productData
      * @param \Generated\Shared\Transfer\LocaleTransfer $localeTransfer
      *
@@ -35,7 +38,7 @@ class ProductImagePageMapExpanderPlugin extends AbstractPlugin implements Produc
      */
     public function expandProductPageMap(PageMapTransfer $pageMapTransfer, PageMapBuilderInterface $pageMapBuilder, array $productData, LocaleTransfer $localeTransfer)
     {
-        $pageMapBuilder->addSearchResultData($pageMapTransfer, 'images', $productData['product_images']);
+        $pageMapBuilder->addSearchResultData($pageMapTransfer, static::KEY, $productData[static::VALUE]);
 
         return $pageMapTransfer;
     }
